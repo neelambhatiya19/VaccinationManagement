4ce15ef3d56d3f5233edd675afde937801adba7b
neelam <neelamb113@gmail.com>
2023-06-20 16:41:34 +0530

    First Commit CitizenService

---
+++ CitizenService/src/test/java/com/demo/project/CitizenService/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.project.CitizenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
